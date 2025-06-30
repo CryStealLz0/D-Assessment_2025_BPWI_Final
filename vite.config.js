b0cb60896e411d4a8f8e291d364be0a9e0c732cb
Dimas Indra Jaya <dimaszxc101@gmail.com>
2025-06-30 23:47:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/D-Assessment_2025_BPW1_Final/',
     root: resolve(__dirname, 'src'),
     publicDir: resolve(__dirname, 'src', 'public'),
     build: {
-        outDir: resolve(__dirname, 'dist'),
+        outDir: resolve(__dirname, 'docs'),
         emptyOutDir: true,
     },
     resolve: {
